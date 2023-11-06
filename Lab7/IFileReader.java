5a556304b674cf6140b741accb1d5643a3441015
besweens <144698652+besweens@users.noreply.github.com>
2023-11-06 14:39:08 -0500

    Initial readfile implementation - testing needed

--- Lab7/IFileReader.java
+++ Lab7/IFileReader.java
@@ -1,8 +1,9 @@
 package Lab7;
 
+import java.io.FileNotFoundException;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
 public interface IFileReader {
-    public TreeMap<String, TreeSet<String>> readFileToTreeMap(String filePath);
+    public TreeMap<String, TreeSet<String>> readFileToTreeMap(String filePath) throws FileNotFoundException;
 }
--- Lab7Main.java
+++ Lab7Main.java
@@ -1,9 +1,13 @@
 import java.io.IOException;
+import java.io.*;
+import java.util.*;
 import Lab7.*;
 public class Lab7Main {
     public static void main(String[] args) throws IOException {
         System.out.print("Test");
 
+        TreeMap<String, TreeSet<String>> testMap = new TreeMap<>();
+       testMap =  myFileReader.readFileToTreeMap(args[0]);
 
     }
 }
