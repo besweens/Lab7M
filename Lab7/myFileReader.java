fef9e0ab383d0ccb66cdcd1416ac81798fc2b313
besweens <144698652+besweens@users.noreply.github.com>
2023-11-06 14:16:17 -0500

    Initial Commit

---
+++ Lab7/myFileReader.java
@@ -0,0 +1,20 @@
+package Lab7;
+
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class myFileReader implements IFileReader{
+
+    /**
+     * Takes in the filepath to a CSV, reads it to a TreeMap
+     * Key = Country name
+     * Value = remaining columns
+     * @param filePath filename for the desired CSV
+     * @return null
+     */
+    @Override
+    public TreeMap<String, TreeSet<String>> readFileToTreeMap(String filePath) {
+
+        return null;
+    }
+}
--- Lab7Main.java
+++ Lab7Main.java
@@ -1,8 +1,9 @@
 import java.io.IOException;
-
+import Lab7.*;
 public class Lab7Main {
     public static void main(String[] args) throws IOException {
-
         System.out.print("Test");
+
+
     }
 }
